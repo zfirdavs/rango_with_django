2333111cd50bdedf93f09307e3dcd7904a7891d4
PolyMorph <firdavs792@gmail.com>
2019-05-28 23:09:56 +0500

    added add_category and add page functionality

--- rango/urls.py
+++ rango/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'about/$', views.about, name='about'),
     url(r'category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^(?P<category_name_slug>[\w\-]+)/add/$', views.add_page, name='add_page'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Category, Page
+from .forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -21,6 +22,36 @@ def show_category(request, category_name_slug):
     context_dict['category'] = category
 
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    category = get_object_or_404(Category, slug=category_name_slug)
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
 
 
 def about(request):
